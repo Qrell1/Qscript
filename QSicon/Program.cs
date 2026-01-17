8d1be87e27167e747709cc532f35d72423b84558
Qrell1 <133158333+Qrell1@users.noreply.github.com>
2026-01-17 20:55:45 +0300

    QscriptNt0.1
    В данной версии ещё нет транслятора но лексер и парсер полностью готовы! Добавил иконки компилятору и файлам QS ну и приложения с которого для них берутся иконки я сделал мини прогой для его быстрого просмотра

---
+++ QSicon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSicon
+{
+    internal class Program
+    {
+        private static int stringCount;
+
+        static string genSpace (int z)
+        {
+            string str = string.Empty;
+            for (int i = 0; i < z; i++)
+            {
+                str += "~";
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Qscript Watching";
+
+            string[] codes = File.ReadAllLines(args[0]);
+            stringCount = codes.Length.ToString().Length;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"Qscript file{args[0]}");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            //Console.ReadKey();
+            for (int i = 0; i < codes.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(i);
+                Console.Write($"{genSpace(stringCount - i.ToString().Length)}");
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write("|");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                //codes[i] = codes[i].Replace("\t", "~");
+                Console.WriteLine(codes[i]);
+            }
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+        }
+    }
+}
